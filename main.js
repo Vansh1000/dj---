9709e17c88c3a2854fec77e756014954e9dbe792
Vansh1000 <90390748+Vansh1000@users.noreply.github.com>
2022-01-31 18:27:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+var song = "";
+
+function preload(){
+song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(600, 500);
+    video.hide()
+
+}
+
+function draw(){
+  image(video, 0, 0, 600, 500)
+}
+
+function play(){
+song.play();
+}
+
